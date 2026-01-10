e3da621935980f6c65a79db2265feb0d8d44b709
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2026-01-11 01:02:34 +0530

    overlay functionality added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let menuBar = document.querySelector(".bar");
 let asidePage = document.querySelector("aside");
 let menuClose = document.querySelector(".close");
+let overlay = document.querySelector(".overlay");
 
 menuBar.addEventListener("click", () => {
   asidePage.classList.add("open");
+  overlay.classList.add("active")
 });
 
 menuClose.addEventListener("click", () => {
