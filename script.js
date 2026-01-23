5902826526e1f7d480939074fec442b0156b8d3d
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2026-01-24 00:38:29 +0530

    dropdown 1 done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let menuBar = document.querySelector(".bar");
 let asidePage = document.querySelector("aside");
 let menuClose = document.querySelector(".close");
 let overlay = document.querySelector(".overlay");
+let dropdownBtn = document.querySelector(".active-arrow");
+let dropdown = document.querySelector(".dropdown-aside");
 
 menuBar.addEventListener("click", () => {
   asidePage.classList.add("open");
@@ -12,3 +14,7 @@ menuClose.addEventListener("click", () => {
   asidePage.classList.remove("open");
   overlay.classList.remove("active")
 });
+
+dropdownBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("hii");
+})
